19ea5f246c1005023de52371c9db2aa3daf09d15
a9sk <920a9sk42f76c765@proton.me>
2024-06-22 00:30:01 +0200

    added getters setters and tostring

--- Note.java
+++ Note.java
@@ -9,7 +9,46 @@ public class Note implements Serializable {
         this.title = title;
         this.content = content;
         this.category = category;
-
     }
 
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        this.dateModified = new Date();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        this.dateModified = new Date();
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+        this.dateModified = new Date();
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getDateModified() {
+        return dateModified;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (" + category + ")";
+    }
 }
